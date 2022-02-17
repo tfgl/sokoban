22037029122014c417b63af8b88446f33884d1e6
tfgl <tafanelgil@gmail.com>
2022-02-17 18:17:54 +0100

    sdl

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
-#define W 5
+#define W 10
 #define H 5
 
 typedef enum {UP, LEFT, DOWN, RIGHT} DIR;
@@ -18,15 +20,16 @@ typedef struct WindowStuff {
   SDL_Rect viewPort;
   int width, height, fps;
   char* title;
+  char open;
 } WindowStuff;
 void initWin(WindowStuff* win, int width, int height, char* title);
 
 char map[H][W] = {
-  {'.', '.', '.', '.', '.'},
-  {'.', 'X', '.', '.', '.'},
-  {'.', '.', 'c', '.', '.'},
-  {'.', 'o', 'c', '.', '.'},
-  {'.', '.', '.', '.', '.'}
+  {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+  {'X', '.', 'o', '.', 'X', '.', '.', '.', '.', 'X'},
+  {'X', '.', 'c', '.', '.', '.', 'c', 'o', '.', 'X'},
+  {'X', 'o', 'c', '.', 'X', '.', '.', '.', '.', 'X'},
+  {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
 };
 
 void newPlayer(pos p, int x, int y) {
@@ -108,7 +111,7 @@ int move(pos* p, DIR d) {
       break;
   }
   // if moved successfully
-  if( (*p)[0] == p2[0] && (*p)[1] == p2[1] ) {
+  if( (*p)[0] == p2[0] && (*p)[1] == p2[1] && !((*next == 'o' && *curent == '.') || (*next == '.' && *curent == 'o'))) {
     *next = *curent;
     *curent = '.';
     return 1;
@@ -117,42 +120,113 @@ int move(pos* p, DIR d) {
   return 0;
 }
 
-void render(pos* p) {
+void render(pos p, WindowStuff* win) {
+  SDL_Surface* surf = IMG_Load("textures.png");
+  SDL_Texture* textures = SDL_CreateTextureFromSurface(win->renderer, surf);
+  int size = 96;
+
   for(int i = 0; i < H; i++) {
     for(int j = 0; j < W; j++) {
-      if( (*p)[0] == j && (*p)[1] == i)
-        putchar('P');
-      else
-        putchar( map[i][j] );
+      SDL_Rect src, dst = {.x = j*size, .y = i*size, .w = size, .h = size};
+      src.h = 32;
+      src.w = 32;
+      switch( map[i][j] ) {
+        case '.':
+          src.x = 32;
+          src.y = 0;
+          break;
+
+        case 'X':
+          src.x = 0;
+          src.y = 0;
+          break;
+
+        case 'o':
+          src.x = 0;
+          src.y = 64;
+          break;
+
+        case 'c':
+          src.x = 32;
+          src.y = 64;
+          break;
+
+        case '#':
+          src.x = 64;
+          src.y = 64;
+          break;
+      }
+
+      SDL_RenderCopy( win->renderer, textures, &src, &dst);
+
+      if( p[0] == j && p[1] == i) {
+        src.x = 64;
+        src.y = 0;
+        SDL_RenderCopy( win->renderer, textures, &src, &dst);
+      }
     }
-    putchar('\n');
   }
-  putchar('\n');
+
+  SDL_RenderPresent( win->renderer );
+}
+
+void keydown(SDL_Keycode k, pos* p) {
+  switch(k) {
+    case SDLK_COMMA:
+      move(p, UP);
+      break;
+
+    case SDLK_a:
+      move(p, LEFT);
+      break;
+
+    case SDLK_e:
+      move(p, RIGHT);
+      break;
+
+    case SDLK_o:
+      move(p, DOWN);
+      break;
+  }
+}
+
+void keyup(SDL_Keycode k, pos* p) {
 }
 
 int main() {
   WindowStuff win;
-  initWin(&win, 500, 500, "sokoban");
+  initWin(&win, 1000, 1000, "sokoban");
+  pos p;
+  newPlayer(p, 3, 2);
 
-  while(1) {
+  while( win.open ) {
+    double t0 = SDL_GetTicks();
+    SDL_RenderClear( win.renderer );
+
+    SDL_Event e;
+    while( SDL_PollEvent(&e) ) {
+      switch( e.type ) {
+        case SDL_QUIT:
+          win.open = 0;
+          break;
+
+        case SDL_KEYDOWN:
+          keydown(e.key.keysym.sym, &p);
+          break;
+
+        case SDL_KEYUP:
+          keyup(e.key.keysym.sym, &p);
+          break;
+      }
+    }
+
+    render(p, &win);
+
+    double delay = win.fps - (SDL_GetTicks() - t0);
+    if(delay > 0)
+      SDL_Delay(delay);
   }
 
-  pos* p = malloc(sizeof(pos));
-  newPlayer(*p, 3, 2);
-  render(p);
-  move(p, UP);
-  render(p);
-  move(p, LEFT);
-  render(p);
-  move(p, DOWN);
-  render(p);
-  move(p, RIGHT);
-  move(p, DOWN);
-  render(p);
-  move(p, LEFT);
-  move(p, LEFT);
-  move(p, UP);
-  render(p);
   return 0;
 }
 
@@ -160,6 +234,8 @@ void initWin(WindowStuff* win, int width, int height, char* title) {
   win->width = width;
   win->height = height;
   win->fps = 60;
+  win->open = 1;
+  win->title = malloc(sizeof(title));
   strcpy(win->title, title);
 
   win->viewPort.w = win->width;
@@ -172,12 +248,10 @@ void initWin(WindowStuff* win, int width, int height, char* title) {
       SDL_WINDOWPOS_UNDEFINED,
       win->width,
       win->height,
-      SDL_WINDOW_SHOWN
-    );
+      SDL_WINDOW_SHOWN);
 
   win->renderer = SDL_CreateRenderer(
       win->window,
       -1,
-      SDL_RENDERER_ACCELERATED
-    );
+      SDL_RENDERER_ACCELERATED);
 }
